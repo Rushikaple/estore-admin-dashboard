8944f23583f5473214e860cc942b8f79533e35c8
Rushikaple <rushikesh.kaplesrk@gmail.com>
2023-07-03 15:37:51 +0000

    File Modified

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,6 @@
 pipeline {
     agent any
-    tools {nodejs "NodeJS"}
+    tools {node.js "NodeJS"}
     stages {
         stage('Source') {
             steps {
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -15,14 +15,14 @@ describe('Test Admin DashBoard', ()=>{
   it('should login with email and password', function(){
     cy.get('input[formControlName=email]').type("admin@example.com")
     cy.get('input[formControlName=password]').type("admin123")
-    cy.get('.btn').click()
+    cy.get('button.btn-primary').click()
     cy.get('.navbar-nav li').should('have. length', 6)
   });
   
   it('should redirect admin user to the dashboard page if they provided correct credentials', ()=>{
     cy.get('input[formControlName=email]').type("admin@example.com")
     cy.get('input[formControlName=password]').type("admin123")
-    cy.get('#loginBtn').click()
+    cy.get('button.btn-primary').click()
     cy.url().should('be.equal', 'http://localhost:4200/users')
   })
 })
